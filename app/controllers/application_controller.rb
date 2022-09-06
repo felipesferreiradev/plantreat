ddd7d9b5ae75f13ed558bf2d3f72c55f2b2613cd
Silvia Neves <siltatiana.sn@gmail.com>
2022-09-06 18:27:57 +0100

    fixes schema issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :score, :has_plant])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :score, :has_plant])
   end
 end
--- db/migrate/20220905155245_devise_create_users.rb
+++ db/migrate/20220905155245_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.boolean :has_plant
+      t.boolean :has_plant, default: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
