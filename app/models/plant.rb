b333662d57c3a995a14420b68ad8e37173be0458
Silvia Neves <siltatiana.sn@gmail.com>
2022-09-07 17:02:02 +0100

    adds seeds

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,5 @@
 class Plant < ApplicationRecord
-  belongs_to :user
-
+  has_many :logs
   has_one_attached :image
   validates :name, presence: true
   validates :has_plant, inclusion: [true, false]
