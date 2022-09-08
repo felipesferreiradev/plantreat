ae4203679f71d124f5eb17c6a11ddedcea278cd1
Silvia Neves <siltatiana.sn@gmail.com>
2022-09-08 12:01:06 +0100

    resets seeds

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_215215) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.boolean "has_plant"
+    t.boolean "has_plant", default: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_215215) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "score"
+    t.integer "score", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
