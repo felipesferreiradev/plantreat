648ee684badad18b4debf1bd65a52fd3c2e07afe
Silvia Neves <siltatiana.sn@gmail.com>
2022-09-13 21:37:20 +0100

    styles profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  # has_merit
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :logs
   has_many :plants, through: :logs, dependent: :destroy
 
   validates :username, presence: true
 
-
+  # has_merit
   # after_create :ensure_points_to_user_on_signup
-
   private
-
     def ensure_points_to_user_on_signup
       self.add_points(100)
     end
