40b98490151b47f34b33ca5b11c1d49360438f36
Felipe Ferreira <lfelipesferreira@gmail.com>
2022-09-13 15:30:35 +0200

    Added merit do the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
   has_many :plants, through: :logs, dependent: :destroy
 
   validates :username, presence: true
+
+
+  after_create :ensure_points_to_user_on_signup
+
+  private
+
+    def ensure_points_to_user_on_signup
+      self.add_points(100)
+    end
 end
