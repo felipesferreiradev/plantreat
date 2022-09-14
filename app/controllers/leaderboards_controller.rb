055dea1b0f4bce9f08cdf6f687d4e41dc89d8e41
Felipe Ferreira <lfelipesferreira@gmail.com>
2022-09-14 22:04:14 +0200

    Leaderboard Almost done

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,5 +2,6 @@ class LeaderboardsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :leaderboard]
   def leaderboard
     @user = User.all
+    @users_orders_by_points = User.all.sort_by { |user| user.points }.reverse
   end
 end
